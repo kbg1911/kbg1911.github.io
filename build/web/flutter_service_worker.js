41d93914b357a01d6e3be591645d9bd78c01237b
BongGu Kang <kbg1911@gmail.com>
2022-08-02 12:23:18 +0900

    bugfix

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -18,8 +18,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "1298de9811bc2522125dfc23c57da90a",
-"/": "1298de9811bc2522125dfc23c57da90a",
+"index.html": "5b5a1e8a7c6e7af27f2b8f782e8a40c4",
+"/": "5b5a1e8a7c6e7af27f2b8f782e8a40c4",
 "main.dart.js": "8ab6e3a56e3d2e8be2b483c6204813d8",
 "manifest.json": "721b5e8359bda554b07968873c835419",
 "version.json": "4a1e0652efc4d76b91515afd5123e5a3"
