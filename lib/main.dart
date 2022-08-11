d788324435b6fc3fbb05c5a258b46f9324ee8b75
BongGu Kang <kbg1911@gmail.com>
2022-08-11 10:53:32 +0900

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
-import 'search.dart';
-import 'bbs.dart';
-import 'write.dart';
+import 'class/ItemProductStatefulState.dart';
+import 'view/search.dart';
+import 'widgets/bbs.dart';
+import 'view/write.dart';
 
 import 'class/common.dart';
 
 List itemList = <Bbs_Data>[];
 
 void main() {
-  itemList.add(Bbs_Data('title','content','writer'));
-  itemList.add(Bbs_Data('제목','내용','글쓴이'));
   itemList.add(Bbs_Data('냉장고','한번만씀','강님'));
   itemList.add(Bbs_Data('맥북','공짜로드림','백님'));
   itemList.add(Bbs_Data('고양이','꾹꾹이','윤님'));
+  itemList.add(Bbs_Data('건강이최고','건강 잘 챙기세요','국님'));
+  itemList.add(Bbs_Data('기타','등등','강님'));
+  itemList.add(Bbs_Data('기타','등등','강님'));
+  itemList.add(Bbs_Data('기타','등등','강님'));
 
   runApp(
     const MaterialApp(title: 'basicApp', home: MyApp()),
@@ -86,7 +89,7 @@ class MyApp extends StatelessWidget {
           ),
         ),
         floatingActionButton: FloatingActionButton(
-            child: Icon(Icons.add),
+            child: const Icon(Icons.add),
             onPressed: () => {
               Navigator.push(
                 context, MaterialPageRoute(builder: (context) => Write())
@@ -97,46 +100,4 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class ItemProductStateful extends StatefulWidget {
-  const ItemProductStateful({Key? key}) : super(key: key);
 
-  @override
-  State<ItemProductStateful> createState() => _ItemProductStatefulState();
-}
-
-class _ItemProductStatefulState extends State<ItemProductStateful> {
-  var textLabel2 = 5;
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      width: double.infinity,
-      height: 100,
-      child: Row(
-        children: [
-          IconButton(onPressed: onClickNone1, icon: const Icon(Icons.plus_one)),
-          IconButton(
-              onPressed: onClickNone2,
-              icon: const Icon(Icons.exposure_minus_1)),
-          Text(textLabel2.toString()),
-        ],
-      ),
-    );
-  }
-
-  void onClickNone1() {
-    setState(() {
-      if (textLabel2 < 99) {
-        textLabel2++;
-      }
-    });
-  }
-
-  void onClickNone2() {
-    setState(() {
-      if (textLabel2 > 0) {
-        textLabel2--;
-      }
-    });
-  }
-}
---
+++ lib/widgets/bbs.dart
@@ -0,0 +1,78 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class Bbs extends StatelessWidget {
+  var title ;
+  var content ;
+  var writer ;
+
+  Bbs({
+    Key? key,
+    this.title,
+    this.content,
+    this.writer
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(border: Border.all(width: 1, color: Colors.red)),
+      width: double.infinity,
+      height: 150,
+      child: TextButton (
+        child: Row(
+          children: [
+            Flexible(
+                fit: FlexFit.tight,
+                flex: 3,
+                child: Container (
+                  decoration: BoxDecoration(border: Border.all(width: 1, color: Colors.black)),
+
+                  padding: const EdgeInsets.all(10),
+                  child: const Image(image: AssetImage('Images/product.jpg')),
+                )
+            ),
+            Flexible(
+              fit: FlexFit.tight,
+              flex: 7,
+              child: Column(
+                children: [
+                  Container (
+                    decoration: BoxDecoration(border: Border.all(width: 2, color: Colors.black)),
+                    height: 35,
+                    padding: const EdgeInsets.all(5),
+                    alignment: Alignment.centerLeft,
+
+                    child: Text(title),
+                  ),
+                  Expanded (
+                      child: Container (
+                        decoration: BoxDecoration(border: Border.all(width: 2, color: Colors.black)),
+                        padding: EdgeInsets.all(5),
+                        alignment: Alignment.topLeft,
+                        child: Text(content),
+                      )
+                  ),
+
+                  Container (
+                    height: 20,
+                    alignment: Alignment.centerRight,
+                    color: Colors.yellow,
+                    child: Text(writer),
+
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+        onPressed: () {
+          print("Click"+title);
+        },
+      )
+
+
+    );
+  }
+}
